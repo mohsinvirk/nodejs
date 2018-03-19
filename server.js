e780c01acf27ac4f89ad0836dc04a92757f6a905
Mohsin Latif <mohsinlatifvirk@gmail.com>
2018-03-19 22:31:00 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res, next)=>{
+    const now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url} `;
+    fs.appendFile('server.log', log + '\n', (err)=> {
+        if (err) {
+            console.log('Something went Wrong');
+        }
+    })
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage:'Wellcome to the Home Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+        currentYear: new Date().getFullYear()
+    });
+
+}); 
+app.get('/bad', (req, res) => {
+     res.json({
+         response:'Bad URL, Please type a correct URL'
+     });
+});
+
+
+app.listen(3000, () => {
+    console.log(`Server started on 3000`);
+});
